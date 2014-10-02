ac282cea613c69fd1bf5bd0f59009f31a316fa46
Skkeem <skkeem@gmail.com>
2014-10-02 21:53:14 +0900

    done

--- assignment/assignment.ino
+++ assignment/assignment.ino
@@ -1,22 +1,92 @@
-
-void setup()
+#include <Servo.h>                           // Include servo library
+ 
+Servo servoLeft;                             // Declare left and right servos
+Servo servoRight;
+int k = 0;
+ 
+void setup()                                 // Built-in initialization block
 {
-  
+  pinMode(3, INPUT);  pinMode(2, OUTPUT);    // Right IR LED & Receiver
+  pinMode(8, OUTPUT);
+
+  servoLeft.attach(13);                      // Attach left signal to pin 13
+  servoRight.attach(12);                     // Attach right signal to pin 12
+}  
+ 
+void loop()                                  // Main loop auto-repeats
+{
+  int irFront = irDetect(2, 3, 38000);       // Check for object on right
+
+  if (irFront == 0) {
+    digitalWrite(8, HIGH);
+    pause(1000);
+    irFront == irDetect(2, 3, 38000);
+    
+    if (irFront == 0) {
+      if (k == 0) {
+        turnRight(600);
+        forward(100);
+        k = 1;
+      }
+      else if (k == 1) {
+        turnLeft(500);
+        turnLeft(500);
+        forward(100);
+        k = 2;
+      }
+      else {
+        turnLeft(600);
+        k = 0;
+      }
+    }
+  }
+  else {
+    digitalWrite(8, LOW);
+    forward(100);
+    k = 0;
+  }
 }
 
-void loop()
+void pause(int time)                       // Forward function
 {
-	if (count == 4) {
-		rotate(180);
-		count = 0;		
-	}
-	if (count == 2 || detected ()) {
-		rotate(90);
-		count++;
-	}
-	else {
-		forward();
-		count = 0;
-	}
+  servoLeft.writeMicroseconds(1500);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1500);        // Right wheel clockwise
+  delay(time);                               // Maneuver for time ms
 }
 
+int irDetect(int irLedPin, int irReceiverPin, long frequency)
+{
+  tone(irLedPin, frequency, 8);              // IRLED 38 kHz for at least 1 ms
+  delay(1);                                  // Wait 1 ms
+  int ir = digitalRead(irReceiverPin);       // IR receiver -> ir variable
+  delay(1);                                  // Down time before recheck
+  return ir;                                 // Return 1 no detect, 0 detect
+}  
+
+void forward(int time)                       // Forward function
+{
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  delay(time);                               // Maneuver for time ms
+}
+
+void turnLeft(int time)                      // Left turn function
+{
+  servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1300);        // Right wheel clockwise
+  delay(time);                               // Maneuver for time ms
+}
+
+void turnRight(int time)                     // Right turn function
+{
+  servoLeft.writeMicroseconds(1700);         // Left wheel counterclockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
+  delay(time);                               // Maneuver for time ms
+}
+
+void backward(int time)                      // Backward function
+{
+  servoLeft.writeMicroseconds(1300);         // Left wheel clockwise
+  servoRight.writeMicroseconds(1700);        // Right wheel counterclockwise
+  delay(time);                               // Maneuver for time ms
+}
